a1442bada5b9525999a8a48941813d99595d12dd
Tayyab Shahzad <tayyab.shahzad100000@gmail.com>
2024-08-05 13:30:49 +0500

    static popcorn done

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,23 @@ import StarRating from "./Components/StarRating";
 import App from "./App.jsx";
 import "./index.css";
 
+const Test = () => {
+  const [rating, setRating] = React.useState(0);
+  return (
+    <div>
+      <StarRating color="blue" MaxRating={10} onSettingRating={setRating} />
+      <p>User rated this movie {rating} stars</p>
+    </div>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    {/* <StarRating /> */}
+    {/* <StarRating
+      MaxRating={5}
+      Message={["Terrible", "Okay", "Good", "Amzaing", "Best"]}
+    /> */}
+    <Test />
   </React.StrictMode>
 );
